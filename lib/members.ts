d1adb990f74b4e0e326dd2c2ffeb298c281883ec
sachitkumar2 <kumarsachit2007@gmail.com>
2026-09-08 05:16:38 -0700

    test user added

--- lib/members.ts
+++ lib/members.ts
@@ -115,6 +115,8 @@ export const BASE_MEMBERS: Member[] = [
   { email: "shipra_jha1@berkeley.edu", firstName: "Shipra", lastName: "Jha", phone: "", group: "H", roles: roles({  }) },
   { email: "surabhikhanna@berkeley.edu", firstName: "Surabhi", lastName: "Khanna", phone: "", group: "E", roles: roles({ lead: true }) },
   { email: "viploverahate@berkeley.edu", firstName: "Viplove", lastName: "Rahate", phone: "", group: "R", roles: roles({  }) }
+  { email: "mtoabstudios@gmail.com", firstName: "Sachit", lastName: "Youtube", phone: "", group: "R", roles: roles({  }) }
+
 ];
 
 /**
